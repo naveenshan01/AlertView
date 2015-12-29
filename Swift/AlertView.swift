ce5ea27b16654c748d8d317c36d1ac80845cb5bf
Naveen Shan <naveenshan@Naveens-MacBook-Pro.local>
2015-12-29 22:54:29 +0530

    Add UIAlertController category method to showInController:

--- Swift/AlertView.swift
+++ Swift/AlertView.swift
@@ -12,7 +12,7 @@ public class AlertView: NSObject {
 
     public typealias AlertCompletionHandler = (buttonIndex: NSInteger, alertController: UIAlertController)->Void
     
-    //MARK - Type methods
+    //MARK:- Type methods
     
     private class func presentationContorller() -> UIViewController? {
         var topViewContorller = UIApplication.sharedApplication().keyWindow?.rootViewController
@@ -37,7 +37,7 @@ public class AlertView: NSObject {
         return topViewContorller;
     }
     
-    //MARK - Type methods
+    //MARK:- Type methods
     
     public class func showAlert(alertMessage: String) {
         AlertView.showAlert("", message: alertMessage, completionHandler: nil)
@@ -52,7 +52,7 @@ public class AlertView: NSObject {
     }
     
     public class func showAlert(alertTitle: String, message alertMessage: String, completionHandler: AlertCompletionHandler?) {
-        AlertView.showAlert(alertTitle, message: alertMessage, okButtonTitle: NSLocalizedString("Ok", comment: ""), cancelButtonTitle: nil, completionHandler: completionHandler)
+        AlertView.showAlert(alertTitle, message: alertMessage, okButtonTitle: NSLocalizedString("OK", comment: ""), cancelButtonTitle: nil, completionHandler: completionHandler)
     }
     
     public class func showAlert(alertTitle: String, message alertMessage: String, var okButtonTitle: (String)?, cancelButtonTitle: (String)?, completionHandler: AlertCompletionHandler?)  {
@@ -91,7 +91,7 @@ public class AlertView: NSObject {
             })
             alertController.addAction(cancelButton)
         } else {
-            okButtonTitle = NSLocalizedString("Ok", comment: "")
+            okButtonTitle = NSLocalizedString("OK", comment: "")
             let okButton = UIAlertAction(title: okButtonTitle, style: UIAlertActionStyle.Default, handler:{ (actionSheetController) -> Void in
                 if ((completionHandler) != nil) {
                     completionHandler!(buttonIndex: 0,alertController: alertController)
@@ -103,5 +103,81 @@ public class AlertView: NSObject {
         
         AlertView.presentationContorller()!.presentViewController(alertController, animated: true, completion: nil)
     }
-  
+    
+    //MARK:- Alert methods
+    
+    public class func alert(alertMessage: String) -> UIAlertController {
+        return AlertView.alert("", message: alertMessage, completionHandler: nil)
+    }
+    
+    public class func errorAlert(error: NSError) -> UIAlertController {
+        return AlertView.alert(NSLocalizedString("Error", comment: ""), message: error.localizedDescription, completionHandler: nil)
+    }
+    
+    public class func alert(alertTitle: String, message alertMessage: String) -> UIAlertController {
+        return AlertView.alert(alertTitle, message: alertMessage, completionHandler: nil)
+    }
+    
+    public class func alert(alertTitle: String, message alertMessage: String, completionHandler: AlertCompletionHandler?) -> UIAlertController {
+        return AlertView.alert(alertTitle, message: alertMessage, okButtonTitle: NSLocalizedString("OK", comment: ""), cancelButtonTitle: nil, completionHandler: completionHandler)
+    }
+    
+    public class func alert(alertTitle: String, message alertMessage: String, var okButtonTitle: (String)?, cancelButtonTitle: (String)?, completionHandler: AlertCompletionHandler?) -> UIAlertController {
+        let alertController = UIAlertController.init(title: alertTitle, message: alertMessage, preferredStyle: UIAlertControllerStyle.Alert)
+        
+        if ((okButtonTitle != nil) && (cancelButtonTitle != nil)) {
+            let okButton = UIAlertAction(title: okButtonTitle, style: UIAlertActionStyle.Default, handler:{ (actionSheetController) -> Void in
+                if ((completionHandler) != nil) {
+                    completionHandler!(buttonIndex: 0,alertController: alertController)
+                }
+                alertController.dismissViewControllerAnimated(true, completion: nil)
+            })
+            
+            let cancelButton = UIAlertAction(title: cancelButtonTitle, style: UIAlertActionStyle.Cancel, handler:{ (actionSheetController) -> Void in
+                if ((completionHandler) != nil) {
+                    completionHandler!(buttonIndex: 1,alertController: alertController)
+                }
+                alertController.dismissViewControllerAnimated(true, completion: nil)
+            })
+            alertController.addAction(okButton)
+            alertController.addAction(cancelButton)
+        } else if (okButtonTitle != nil) {
+            let okButton = UIAlertAction(title: okButtonTitle, style: UIAlertActionStyle.Default, handler:{ (actionSheetController) -> Void in
+                if ((completionHandler) != nil) {
+                    completionHandler!(buttonIndex: 0,alertController: alertController)
+                }
+                alertController.dismissViewControllerAnimated(true, completion: nil)
+            })
+            alertController.addAction(okButton)
+        } else if (cancelButtonTitle != nil) {
+            let cancelButton = UIAlertAction(title: cancelButtonTitle, style: UIAlertActionStyle.Cancel, handler:{ (actionSheetController) -> Void in
+                if ((completionHandler) != nil) {
+                    completionHandler!(buttonIndex: 0,alertController: alertController)
+                }
+                alertController.dismissViewControllerAnimated(true, completion: nil)
+            })
+            alertController.addAction(cancelButton)
+        } else {
+            okButtonTitle = NSLocalizedString("OK", comment: "")
+            let okButton = UIAlertAction(title: okButtonTitle, style: UIAlertActionStyle.Default, handler:{ (actionSheetController) -> Void in
+                if ((completionHandler) != nil) {
+                    completionHandler!(buttonIndex: 0,alertController: alertController)
+                }
+                alertController.dismissViewControllerAnimated(true, completion: nil)
+            })
+            alertController.addAction(okButton)
+        }
+        
+        return alertController
+    }
 }
+
+//MARK:- UIAlertController Extentions
+
+public extension UIAlertController {
+    
+    public func showInController(controller: UIViewController) {
+        controller.presentViewController(self, animated: true, completion: nil)
+    }
+}
+
